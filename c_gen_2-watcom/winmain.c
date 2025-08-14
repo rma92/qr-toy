1316c01d8da14f822f3e818fe937f18d6a97c4f5
user <user@rm.vg>
2025-08-14 18:30:40 +0000

    improved windows port

--- c_gen_2-watcom/winmain.c
+++ c_gen_2-watcom/winmain.c
@@ -5,7 +5,7 @@
 
 #define MAX_DATA_LEN 4096
 #define QR_SCALE_MIN 2
-#define QR_SCALE_MAX 20
+#define QR_SCALE_MAX 2000
 
 QRCode qrcode;
 char qrcodeData[MAX_DATA_LEN];
@@ -44,59 +44,63 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
       windowWidth = LOWORD(lParam);
       windowHeight = HIWORD(lParam);
       InvalidateRect(hwnd, NULL, TRUE);
-      MessageBox(0,"A",0,0);
       return 0;
 
     case WM_PAINT: 
       {
-        int qrSize;
+        int qrSize = qrcode.size;
         int scale;
         int bmpWidth;
         int bmpHeight;
         int x;
         int y;
+        int scaleX;
+        int scaleY;
         int xOffset;
         int yOffset;
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps);
         HDC memDC = CreateCompatibleDC(hdc);
 
-        qrSize = qrcode.size;
-        scale = min(windowWidth / qrSize, windowHeight / qrSize);
-        scale = max(QR_SCALE_MIN, min(scale, QR_SCALE_MAX));
+    // Calculate dynamic scale
+    scaleX = windowWidth / qrSize;
+    scaleY = windowHeight / qrSize;
+    scale = min(scaleX, scaleY);
+    scale = max(QR_SCALE_MIN, min(scale, QR_SCALE_MAX));
 
-        bmpWidth = qrSize * scale;
-        bmpHeight = qrSize * scale;
+    bmpWidth = qrSize * scale;
+    bmpHeight = qrSize * scale;
 
-        HBITMAP bmp = CreateCompatibleBitmap(hdc, bmpWidth, bmpHeight);
-        SelectObject(memDC, bmp);
+    HDC memDC = CreateCompatibleDC(hdc);
+    HBITMAP bmp = CreateCompatibleBitmap(hdc, bmpWidth, bmpHeight);
+    SelectObject(memDC, bmp);
 
-        RECT bg = {0, 0, bmpWidth, bmpHeight};
-        FillRect(memDC, &bg, (HBRUSH)(COLOR_WINDOW + 1));
+    RECT bg = {0, 0, bmpWidth, bmpHeight};
+    FillRect(memDC, &bg, (HBRUSH)(COLOR_WINDOW + 1));
 
-        for (y = 0; y < qrSize; y++) {
-          for (x = 0; x < qrSize; x++) {
+    for (y = 0; y < qrSize; y++) {
+        for (x = 0; x < qrSize; x++) {
             if (qrcode_getModule(&qrcode, x, y)) {
-              RECT r = {
-                x * scale,
-                y * scale,
-                (x + 1) * scale,
-                (y + 1) * scale
-              };
-              FillRect(memDC, &r, (HBRUSH)GetStockObject(BLACK_BRUSH));
+                RECT r = {
+                    x * scale,
+                    y * scale,
+                    (x + 1) * scale,
+                    (y + 1) * scale
+                };
+                FillRect(memDC, &r, (HBRUSH)GetStockObject(BLACK_BRUSH));
             }
-          }
         }
+    }
 
-        xOffset = (windowWidth - bmpWidth) / 2;
-        yOffset = (windowHeight - bmpHeight) / 2;
-        BitBlt(hdc, xOffset, yOffset, bmpWidth, bmpHeight, memDC, 0, 0, SRCCOPY);
+    xOffset = (windowWidth - bmpWidth) / 2;
+    yOffset = (windowHeight - bmpHeight) / 2;
+    BitBlt(hdc, xOffset, yOffset, bmpWidth, bmpHeight, memDC, 0, 0, SRCCOPY);
 
-        DeleteObject(bmp);
-        DeleteDC(memDC);
-        EndPaint(hwnd, &ps);
-        return 0;
-      }
+    DeleteObject(bmp);
+    DeleteDC(memDC);
+    EndPaint(hwnd, &ps);
+    return 0;
+}
 
     case WM_DESTROY:
       PostQuitMessage(0);
