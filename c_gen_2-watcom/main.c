12aed26fc84db16032bd002dfeff770efdcdab4a
user <user@rm.vg>
2025-08-14 16:03:47 +0000

    c_gen_2-watcom now prints using half height chars

--- c_gen_2-watcom/main.c
+++ c_gen_2-watcom/main.c
@@ -7,16 +7,32 @@ void main()
 {
   char x = 0;
   char y = 0;
+  char s[255];
+//int8_t qrcode_initText(QRCode *qrcode, uint8_t *modules, uint8_t version, uint8_t ecc, const char *data)
+  char* qrcodeData = (char*)malloc(qrcode_getBufferSize(8));
+  qrcode_initText(&qrcode, qrcodeData, 8, 0, "HTTP://RM.VG/");
 
-  char* qrcodeData = (char*)malloc(qrcode_getBufferSize(3));
-  qrcode_initText(&qrcode, qrcodeData, 3, 0, "HELLO WORLD");
-
-  for (y = 0; y < qrcode.size; y++)
+  for (y = 0; y < qrcode.size; y+=2)
   {
+    if( y != 0 )printf("\n");    
     for (x = 0; x < qrcode.size; x++)
-    {    
-      printf("%s", qrcode_getModule(&qrcode, x, y) ? "##":"  ");
+    {
+      char o = ' ';
+      if( qrcode_getModule(&qrcode, x, y) & qrcode_getModule(&qrcode, x, y+1) )
+      {
+        o = 219; //full block
+      }
+      else if( qrcode_getModule(&qrcode, x, y) )
+      {
+        o = 223; //only top
+      }
+      else if( qrcode_getModule(&qrcode, x, y+1) )
+      {
+        o = 220; //only bottom
+      }
+
+      printf("%c", o);
     }
-    printf("\n");
   }
+  gets(s);
 }
